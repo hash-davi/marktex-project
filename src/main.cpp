174ba328cc53bf5a0ac467e9209328c7e97b14d9
Davi <davi.castro1319@gmail.com>
2025-03-21 20:48:42 -0300

    working on some new features

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <ctype.h>
+#include <cctype>
+#include <limits>
+#include <ios>
+#include <vector>
 
 using std::string;
 using std::fstream;
@@ -9,6 +12,12 @@ using std::fstream;
 void mainScreen();
 void readLine(string filename, fstream &txtFile);
 void writeLine(string filename, fstream &txtFile);
+int getTextLength(string filename, fstream &txtFile);
+std::vector<string> getTextLines(string filename, fstream &txtFile);
+string getText(string filename, fstream &txtFile);
+void leaveProgram();
+
+constexpr int maxBuffer = 100;
 
 int main() {
 
@@ -22,7 +31,7 @@ void mainScreen() {
     static string filename{};
     char choice{};
 
-    if(filename == "") {
+    if(filename.empty()) {
         std::cout << "Enter filename: ";
         std::cin >> filename;
         filename = "..\\files\\" + filename;
@@ -30,6 +39,8 @@ void mainScreen() {
     else {
         std::cout << "Do you wish to open another file? Type [y] if so or [n] otherwise. ";
         std::cin >> choice;
+
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
         switch(tolower(choice)) {
             case 'y':
@@ -39,11 +50,18 @@ void mainScreen() {
                 break;
             case 'n':
                 break;
+            default:
+                std::cout << "You typed an invalid character. Please try again." << std::endl;
+                system("pause");
+                system("cls");
+                mainScreen();
         }
     }
 
-    std::cout << "Type [r] if you wish to read the file or [w] to write in it: ";
+    std::cout << "Type [r] if you wish to read the file, [w] to write in it or [l] if you wish to leave: ";
     std::cin >> choice;
+
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
     switch(tolower(choice)) {
         case 'r':
@@ -51,6 +69,9 @@ void mainScreen() {
             break;
         case 'w':
             writeLine("..\\files\\test.txt", testFile);
+            break;
+        case 'l':
+            leaveProgram();
             break;
         default:
             std::cout << "You didn't type a valid character. Please try again" << std::endl;
@@ -74,6 +95,8 @@ void readLine(string filename, fstream &txtFile) {
         i = 1;
 
         txtFile.close();
+
+        mainScreen();
     }
     else {
         std::cout << "Error opening file. Please try again.\n";
@@ -84,17 +107,28 @@ void readLine(string filename, fstream &txtFile) {
 }
 
 void writeLine(string filename, fstream &txtFile) {
+    int keyCounter{getTextLength(filename, txtFile) + 1};
+    std::vector<string> textLines = getTextLines(filename, txtFile);
+
     txtFile.open(filename, std::ios_base::app);
 
     if(txtFile.is_open()) {
-        string keyInput{};
+        string textInput{};
 
-        std::cout << "\nInput: ";
+        std::cout << "\nText: ";
 
-        do {
-            std::cin >> keyInput;
-            txtFile << keyInput;
-        } while(!keyInput.compare("\n"));
+        for(int i = 0; i < 50; i++) {
+            std::cout << textLines[i] << '\n';
+        }
+
+        /*
+        getline(std::cin, textInput);
+        fileText += textInput;
+        keyCounter += textInput.length();
+        txtFile << textInput;
+        */
+
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
         txtFile.close();
     }
@@ -107,4 +141,44 @@ void writeLine(string filename, fstream &txtFile) {
 
     system("cls");
     mainScreen();
+}
+
+int getTextLength(string filename, fstream &txtFile) {
+    return getText(filename, txtFile).length();
+}
+
+std::vector<string> getTextLines(string filename, fstream &txtFile) {
+    txtFile.open(filename, std::ios_base::in);
+    string fileText{};
+    std::vector<string> wholeText;
+
+    int i{};
+
+    while(getline(txtFile, fileText)) {
+        wholeText[i] += fileText;
+        ++i;
+    }
+
+    txtFile.close();
+
+    return wholeText;
+}
+
+string getText(string filename, fstream &txtFile) {
+    txtFile.open(filename, std::ios_base::in);
+    string fileText{};
+    string wholeText{};
+
+    while(getline(txtFile, fileText)) {
+        wholeText += fileText;
+    }
+
+    txtFile.close();
+
+    return wholeText;
+}
+
+void leaveProgram() {
+    std::cout << "Thank you for using our app!" << std::endl;
+    exit(0);
 }
