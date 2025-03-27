8ebc6223ad108daf551e23fe8a1321ba8cc6c516
Davi <davi.castro1319@gmail.com>
2025-03-27 20:54:16 -0300

    started FileContent class development

--- src/editorEnvironment.cpp
+++ src/editorEnvironment.cpp
@@ -11,13 +11,42 @@ unsigned long long getCharacterCount(string filename, fstream &txtFile);
 int getLineCount(string filename, fstream &txtFile);
 string getText(string filename, fstream &txtFile);
 
-/*
-int main(){
 
-	return 0;
-}
-*/
+#ifdef FILE_CONTENT_CLASS
+class FileContent {
+    private:
+        fstream file;
+        string fileName{};
+        fstream tempFile(R"(..\\files\\temp\\temporary.txt)");
+        std::vector<string> textLines;
+        uint32_t caretX = 0;
+        uint32_t caretY = 0;
+        uint64_t characterCount{};
+    public:
+        void FileContent(string _filename) {
+            this->file.open(_filename, std::ios_base::in);
+            if(this->file.is_open()) {
+                this->file.close();
+            }
+        }
+        void setFileName(string _filename) {
+            this->fileName = _filename;
+        }
+        string getFileName() {
+            return this->fileName;
+        }
+        void setLines() {
+            string _lineText{};
 
+            int i{};
+            while(getline(this->file, _lineText)) {
+                this->textLines[i++] = _lineText;
+            }
+        }
+};
+#endif // FILE_CONTENT_CLASS
+
+#ifndef FILE_CONTENT_CLASS
 void editor(string filename, fstream &txtFile) {
     string tempFilename{R"(..\\files\\temp\\temporary.txt)"};
     fstream temporaryFile(tempFilename);
@@ -144,3 +173,4 @@ string getText(string filename, fstream &txtFile) {
 
     return fileText;
 }
+#endif
