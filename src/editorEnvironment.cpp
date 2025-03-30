2d0a4282d6289c36e80b26a674b5f743b7a05ec8
Davi <davi.castro1319@gmail.com>
2025-03-30 15:02:31 -0300

    fix: removed some bugs and added some more

--- src/editorEnvironment.cpp
+++ src/editorEnvironment.cpp
@@ -16,113 +16,6 @@ typedef struct {
     uint64_t y{};
 } caretStructure;
 
-#ifdef FILE_CONTENT_CLASS
-class FileContent {
-    private:
-        fstream *file;
-        string fileName{};
-        fstream *tempFile;
-        std::vector<string> textLines;
-        uint32_t caretX = 0;
-        uint32_t caretY = 0;
-        uint64_t characterCount{};
-    public:
-        FileContent(string _filename) {
-            this->file->open(_filename, std::ios_base::in);
-            this->tempFile->open(R"(..\\files\\temp\\temporary.txt)", std::ios_base::in);
-            if(this->file->is_open()) {
-                this->file->close();
-            }
-            if(this->tempFile->is_open()) {
-                this->tempFile->close();
-            }
-        }
-        void openTempFileAppend() {
-            this->tempFile->open(R"(..\\files\\temp\\temporary.txt)", std::ios_base::app);
-        }
-        void closeTempFile() {
-            this->tempFile->close();
-        }
-        fstream *getTempFile() {
-            return this->tempFile;
-        }
-        void setFileName(string _filename) {
-            this->fileName = _filename;
-        }
-        string getFileName() {
-            return this->fileName;
-        }
-        void setTextLines() {
-            string _lineText{};
-            this->file->open(this->getFileName(), std::ios_base::in);
-
-            int i{};
-            while(getline(*this->file, _lineText)) {
-                this->textLines[i++] = _lineText;
-            }
-
-            this->file->close();
-        }
-        std::vector<string> getTextLines() {
-            if(this->textLines.empty()) {
-                this->setTextLines();
-            }
-            return this->textLines;
-        }
-        uint64_t getCharacterCount() {
-            for(auto x: this->getTextLines()) {
-                this->characterCount += x.length();
-            }
-
-            return this->characterCount;
-        }
-        void showFileContent() {
-            std::cout << "File path: " << this->getFileName() << std::endl;
-            std::cout << "Character count: " << this->getCharacterCount();
-
-            uint64_t i{};
-            for(i = 0; i < this->getTextLines().size(); ++i) {
-                std::cout << i + 1 << this->getTextLines().at(i);
-
-                if(i < this->getTextLines().size() - 1) {
-                    std::cout << '\n';
-                }
-            }
-        }
-        uint32_t getCaretX() {
-            return this->caretX;
-        }
-        uint32_t getCaretY() {
-            return this->caretY;
-        }
-        void setCaretPosition() {
-            string lastLine = this->getTextLines().back();
-            uint32_t _caretX = this->getCaretX();
-            _caretX = lastLine.length();
-        }
-};
-
-void editFile(FileContent TxtFile) {
-    char keyInput{};
-
-    TxtFile.setTextLines();
-
-    while(true) {
-        TxtFile.showFileContent();
-
-        if(keyInput == 13) {
-            TxtFile.getTempFile() << '\n';
-        }
-        else if(GetKeyState(VK_BACK) & 0x8000) {
-            if(TxtFile.getCaretX() != 0) {
-                break;
-            }
-        }
-    }
-}
-#endif // FILE_CONTENT_CLASS
-
-#ifndef FILE_CONTENT_CLASS
 void editor(string filename, fstream &txtFile) {
     string tempFilename{R"(..\\files\\temp\\temporary.txt)"};
     fstream temporaryFile(tempFilename);
@@ -153,9 +46,9 @@ void editor(string filename, fstream &txtFile) {
         keyInput = getch();
 
         if(GetKeyState(VK_BACK) < 0) {
-            if(fileText.empty()) {
+            if(fileText.empty() || (caretPosition.x == 0 && caretPosition.y == 1)) {
                 caretPosition.x = 0;
-                caretPosition.y = 0;
+                caretPosition.y = 1;
             }
             else {
                 if(caretPosition.x > 0) {
@@ -208,6 +101,7 @@ void editor(string filename, fstream &txtFile) {
 void showText(string filename, fstream &txtFile) {
     uint64_t charCount{getCharCount(filename, txtFile)};
     uint64_t lineCount{getLineCount(filename, txtFile)};
+    bool hasText{};
 
     txtFile.open(filename, std::ios_base::in);
 
@@ -219,12 +113,16 @@ void showText(string filename, fstream &txtFile) {
     std::cout << "Character count: " << charCount << '\n';
     std::cout << "Line count: " << lineCount << "\n\n"; // Debug
     while(getline(txtFile, buffer)) {
+        hasText = true;
         std::cout << i++ << ": " << buffer;
 
         if(i < lineCount + 1) {
             std::cout << '\n';
         }
 
+    }
+    if(!hasText) {
+        std::cout << i << ": ";
     }
 
     txtFile.close();
@@ -264,10 +162,12 @@ std::vector<string> getTextLines(string filename, fstream &txtFile) {
 
         txtFile.close();
 
+        if(textLines.empty()) {
+            textLines.insert(textLines.begin(), " ");
+        }
         return textLines;
     }
     else {
         exit(-1);
     }
 }
-#endif
